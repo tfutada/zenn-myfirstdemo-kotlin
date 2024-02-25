a1751554d1dd41ba5fbf532ecf0e631e2d066f95
tfutada <tafutada@gmail.com>
2024-02-25 17:05:51 +0900

    Update FanOut.kt to improve coroutine usage
    The main function in FanOut.kt was significantly modified to utilize coroutines more effectively. Specifically, two producer coroutines were created and a "select" statement was used to choose between them. Additionally, a parameter for delay duration was added to the "produceNumbers" function.

--- src/main/kotlin/channels/FanOut.kt
+++ src/main/kotlin/channels/FanOut.kt
@@ -3,15 +3,29 @@ package jp.tf.channels
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.selects.select
+import kotlin.time.Duration
 
-fun main() {
-    println("abc")
+fun main() = runBlocking {
+    val producer1 = produceNumbers(100) // get a producer coroutine
+    val producer2 = produceNumbers(300) // get a producer coroutine
+    repeat(10) {
+        val nextNumber = select<String> {
+            producer1.onReceive { it -> "$it from first" }
+            producer2.onReceive { it -> "$it from second" }
+        }
+
+        println("Received $nextNumber")
+    }
+    producer1.cancel() // cancel producer coroutine because we don't need it anymore
+    producer2.cancel() // cancel producer coroutine because we don't need it anymore
 }
 
-fun CoroutineScope.produceNumbers() = produce<Int> {
+fun CoroutineScope.produceNumbers(duration: Long) = produce<Int> {
     var x = 1 // start from 1
     while (true) {
         send(x++) // produce next
-        delay(100) // wait 0.1s
+        delay(duration) // wait 0.1s
     }
 }
