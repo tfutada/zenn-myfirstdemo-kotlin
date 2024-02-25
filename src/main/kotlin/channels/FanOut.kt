7d3d309bd038ad404279c167c58796131869a5d1
tfutada <tafutada@gmail.com>
2024-02-25 16:42:22 +0900

    Add kotlinx-coroutines-core and coroutine producer function
    The kotlinx-coroutines-core dependency has been added to build.gradle.kts to support concurrent programming. Additionally, a coroutine producer function 'produceNumbers' has been implemented in the FanOut.kt file, which will allow it to produce numbers cyclically.

--- src/main/kotlin/channels/FanOut.kt
+++ src/main/kotlin/channels/FanOut.kt
@@ -1,6 +1,17 @@
 package jp.tf.channels
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+
 fun main() {
     println("abc")
 }
 
+fun CoroutineScope.produceNumbers() = produce<Int> {
+    var x = 1 // start from 1
+    while (true) {
+        send(x++) // produce next
+        delay(100) // wait 0.1s
+    }
+}
