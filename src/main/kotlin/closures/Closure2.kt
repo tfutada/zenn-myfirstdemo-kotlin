e3e4d0f68496afbe36acdd0d1a18f280396bc592
tfutada <tafutada@gmail.com>
2024-02-25 20:34:30 +0900

    Add new Closure2.kt file
    This commit introduces a new Kotlin file, Closure2.kt, which includes a simple closure example for sending money with logging. The file features a main function and a helper function for demonstrating how closures can be used in Kotlin.

---
+++ src/main/kotlin/closures/Closure2.kt
@@ -0,0 +1,18 @@
+package jp.tf.closures
+
+fun main() {
+
+    var counter = 3
+
+    sendMoneyWithLog {
+        counter
+    }
+}
+
+
+fun sendMoneyWithLog(f: () -> Int) {
+    println("in getMoneyWithLog")
+    val money = f()
+    println("send modey: $money")
+}
+
