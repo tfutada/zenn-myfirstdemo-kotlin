47c7178d6800dc4fe5b6ec69cc28e53a5d6c5ddc
tfutada <tafutada@gmail.com>
2024-03-04 12:58:03 +0900

    fan-in

--- src/main/kotlin/fanin/FanIn.kt
+++ src/main/kotlin/fanin/FanIn.kt
@@ -7,15 +7,16 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    val sharedChannel = Channel<Int>() // Create a shared channel for fan-in
+    val sharedChannel = Channel<Int>(10) // Create a shared channel for fan-in
 
     // Launching two producers that send to the same channel
-    launchProducer(sharedChannel, 100) // Producer with 100ms delay
-    launchProducer(sharedChannel, 300) // Producer with 300ms delay
+    launchProducer(sharedChannel, 100, 1) // Producer with 100ms delay
+    launchProducer(sharedChannel, 300, 1000) // Producer with 300ms delay
 
     // Consumer
     repeat(20) { // Assuming we want to consume 20 numbers in total
         println("Received ${sharedChannel.receive()}")
+        delay(300)
     }
 
     sharedChannel.close() // Close the channel when we're done
@@ -23,8 +24,8 @@ fun main() = runBlocking {
     println("Done")
 }
 
-fun CoroutineScope.launchProducer(channel: Channel<Int>, delayTime: Long) = launch {
-    var number = 1
+fun CoroutineScope.launchProducer(channel: Channel<Int>, delayTime: Long, start: Int) = launch {
+    var number = 1 + start
     while (!channel.isClosedForSend) {
         channel.send(number++)
         delay(delayTime)
