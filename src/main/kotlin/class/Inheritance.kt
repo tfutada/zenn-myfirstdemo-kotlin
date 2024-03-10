1d452ab10e9288de55ea7cf8d3220842f66ace88
tfutada <tafutada@gmail.com>
2024-03-10 15:13:05 +0900

    class

--- src/main/kotlin/class/Inheritance.kt
+++ src/main/kotlin/class/Inheritance.kt
@@ -1,11 +1,20 @@
 package jp.tf.`class`
 
-sealed class Base(p: Int)
+sealed class Base(var p: Int) {
+    open fun dump(): String {
+        return "Base(p=$p)"
+    }
 
-class Derived(p: Int) : Base(p)
+}
+
+class Derived(p: Int) : Base(p) {
+    override fun dump(): String {
+        return "Derived(p=$p)"
+    }
+}
 
 fun main() {
     val d = Derived(1)
-    println(d)
+    println(d.dump())
 }
 
