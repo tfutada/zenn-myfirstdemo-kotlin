5b6536876f1ed71c88dc6521991901acfa3b27e4
tfutada <tafutada@gmail.com>
2024-03-10 13:06:29 +0900

    fan-in

---
+++ src/main/kotlin/channel3/PinPon.kt
@@ -0,0 +1,35 @@
+package jp.tf.channel3
+
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+data class Ball(var hits: Int)
+
+// https://kotlinlang.org/docs/channels.html#channels-are-fair
+// probe the fairness of coroutines.
+fun main() = runBlocking {
+    val table = Channel<Ball>() // a shared table
+
+    launch { player("ping", table) }
+    launch { player("pong", table) }
+
+    // let's start the game!
+    table.send(Ball(0)) // serve the ball
+
+
+    delay(1000) // delay 1 second
+    coroutineContext.cancelChildren() // game over, cancel them
+}
+
+// this function is a player in the game, which receives a ball and then sends it back
+suspend fun player(name: String, table: Channel<Ball>) {
+    for (ball in table) { // receive the ball in a loop
+        ball.hits++
+        println("$name $ball")
+        delay(300) // wait a bit
+        table.send(ball) // send the ball back
+    }
+}
